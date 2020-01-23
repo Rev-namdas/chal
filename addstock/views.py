51d1a7ee0d2d008d38dcdd02f8d008c6b6201632
sadman <sadmanahmedsadman@gmail.com>
2020-01-24 04:28:14 +0600

    report updated with calender

--- addstock/views.py
+++ addstock/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from addstock.models import stockModel, productsModel, sellStockModel, remainingModel
-import datetime
+from datetime import datetime
+from datetime import timedelta
+
 
 # Create your views here.
 
@@ -15,7 +17,7 @@ def addstock(request):
         amount = request.POST['amount']
         bosta = request.POST['bosta']
 
-        stocks = stockModel(stockedby=stockedby, rice=rice, amount=amount, bosta=bosta, time = datetime.datetime.now())
+        stocks = stockModel(stockedby=stockedby, rice=rice, amount=amount, bosta=bosta, time = datetime.now())
 
         if stocks is not None:
             stocks.save()
@@ -47,10 +49,6 @@ def stockresult(request, sid):
 
     remainamount = 0
     remainbosta = 0
-    
-    if request.method == 'POST':
-        date = request.POST.get('date')
-        print(date[3:5])
 
     for each in stocks:
         if each.rice == products.product:
@@ -94,7 +92,7 @@ def sellstock(request, sid):
         bosta = request.POST['bosta']
         price = request.POST['price']
 
-        sell = sellStockModel(customer = name, rice = products.product, amount = amount, bosta = bosta, time = datetime.datetime.now(), price = price)
+        sell = sellStockModel(customer = name, rice = products.product, amount = amount, bosta = bosta, time = datetime.now(), price = price)
         
         if sell is not None:
             sell.save()
@@ -144,77 +142,20 @@ def report(request):
     startingdate    = "2020-01-01"
     finishingdate   = "2050-01-01"
 
+    totalamount = 0
+    totalbosta  = 0
+
     if request.method == 'POST':
-        fromdate    = request.POST['fromdate']
-        frommonth   = request.POST['frommonth']
-        fromyear    = request.POST['fromyear']
-
-        todate      = request.POST['todate']
-        tomonth     = request.POST['tomonth']
-        toyear      = request.POST['toyear']
-
-        nfmonth = ''
-
-        if frommonth == 'January':
-            nfmonth = '01'
-        elif frommonth == 'February':
-            nfmonth = '02'
-        elif frommonth == 'March':
-            nfmonth = '03'
-        elif frommonth == 'April':
-            nfmonth = '04'
-        elif frommonth == 'May':
-            nfmonth = '05'
-        elif frommonth == 'June':
-            nfmonth = '06'
-        elif frommonth == 'July':
-            nfmonth = '07'
-        elif frommonth == 'August':
-            nfmonth = '08'
-        elif frommonth == 'September':
-            nfmonth = '09'
-        elif frommonth == 'October':
-            nfmonth = '10'
-        elif frommonth == 'November':
-            nfmonth = '11'
-        elif frommonth == 'December':
-            nfmonth = '12'
-
-
-        ntmonth = ''
-
-        if tomonth == 'January':
-            ntmonth = '01'
-        elif tomonth == 'February':
-            ntmonth = '02'
-        elif tomonth == 'March':
-            ntmonth = '03'
-        elif tomonth == 'April':
-            ntmonth = '04'
-        elif tomonth == 'May':
-            ntmonth = '05'
-        elif tomonth == 'June':
-            ntmonth = '06'
-        elif tomonth == 'July':
-            ntmonth = '07'
-        elif tomonth == 'August':
-            ntmonth = '08'
-        elif tomonth == 'September':
-            ntmonth = '09'
-        elif tomonth == 'October':
-            ntmonth = '10'
-        elif tomonth == 'November':
-            ntmonth = '11'
-        elif tomonth == 'December':
-            ntmonth = '12'
-
-        if fromyear and frommonth and fromdate:
-            startingdate    = fromyear + "-" + nfmonth + "-" + fromdate
+        fromdate    = request.POST['date1']
+        todate      = request.POST['date2']
+        if fromdate:
+            startingdate    = fromdate
         else:
             startingdate    = "2020-01-01"
 
-        if toyear and tomonth and todate:
-            finishingdate   = toyear + "-" + ntmonth + "-" + str(int(todate) + 1)
+        if todate:
+            newtodate       = datetime.strptime(todate, '%Y-%m-%d').date()
+            finishingdate   = newtodate + timedelta(days=1)
         else:
             finishingdate   = "2050-01-01"
         
@@ -222,4 +163,8 @@ def report(request):
 
     sells = sellStockModel.objects.filter(time__range=[startingdate, finishingdate]).order_by('time')
 
-    return render(request, 'report.html', {'sells' : sells})
+    for each in sells:
+        totalamount += each.amount
+        totalbosta  += each.bosta
+
+    return render(request, 'report.html', {'sells' : sells, 'totalamount' : totalamount, 'totalbosta' : totalbosta})
